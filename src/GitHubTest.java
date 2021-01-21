5dd7e6845faab95d37966c7c1a19ccb25afc9541
Youngjin <dnjun2@ajou.ac.kr>
2021-01-21 20:24:08 +0900

    21/01/21 java coding test practice starts!

---
+++ src/GitHubTest.java
@@ -0,0 +1,7 @@
+
+public class GitHubTest {
+	public static void main(String[] args) {
+		System.out.println("Hello My name is Young Jin");
+		System.out.println("Java Coding Test Fighting!!");
+	}
+}
