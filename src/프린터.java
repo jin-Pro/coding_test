ddaa2fadaf511b39c485fc231e6a4be816ed09b4
jin-Pro <70205497+jin-Pro@users.noreply.github.com>
2021-01-30 04:59:16 +0900

    2021-01-30 Programmers Level 2 Stack & Queue

---
+++ src/프린터.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+class 포인터 {
+
+    PriorityQueue<Print> printList = new PriorityQueue<Print>();
+
+    public int solution(int[] priorities, int location) {
+        //정답의 횟수를 카운트할 변수
+        int answer = 0;
+
+        //요청한 로케이션의 값이 나갔다는 표시
+        boolean isOut = false;
+
+        for(int i=0; i<priorities.length;i++) {
+            printList.offer(new Print(i, priorities[i]));
+        }
+
+
+        while (!isOut) {
+            if(printList.poll().getLocation() == location) {
+                isOut = true;
+            }
+            answer++;
+        }
+
+        return answer;
+    }
+}
+
+class Print implements Comparable<Print>{
+
+    int location;
+    int  priority;
+
+    public Print(int location, int priority) {
+        this.location = location;
+        this.priority = priority;
+    }
+
+
+    public int getLocation() {
+        return this.location; 
+    }
+
+    public int getPriority() {
+        return this.priority; 
+    }
+
+    public String toString() {
+        return "Print [location=" + location + ", priority=" + priority + "]";
+    }
+
+
+    @Override
+    public int compareTo(Print o) {
+        // TODO Auto-generated method stub
+        return o.priority -this.priority;
+    }
+}
