7a07d7903cf878ab6cf61fef288003c14f990c0b
jin-Pro <dnjun2@ajou.ac.kr>
2021-03-22 17:18:17 +0900

    2021-03-22 AjouCodingStudy No_01

---
+++ AjouStudy/No_01.py
@@ -0,0 +1,100 @@
+def gameOver(x,y,snake):
+    if x == -1 or y == -1 or x == matrix or y == matrix:
+        return True
+
+    if [x,y] in snake:
+        return True    
+    
+    return False
+
+def catchApple(x,y,apple):
+
+    if apple[x][y]:
+        apple[x][y] = False
+        return True
+    
+    return False
+
+
+def changeDir(direction,i):
+    if i[1] == 'D' :
+        if direction == [0,1]:
+            direction[0] = 1
+            direction[1] = 0
+        
+        elif direction == [1,0]:
+            direction[0] = 0
+            direction[1] = -1
+        
+        elif direction == [0,-1]:
+            direction[0] = -1
+            direction[1] = 0
+        
+        elif direction == [-1,0]:
+            direction[0] = 0
+            direction[1] = 1
+
+    elif i[1] == 'L' :
+        if direction == [0,1]:
+            direction[0] = -1
+            direction[1] = 0
+
+        elif direction == [1,0]:
+            direction[0] = 0
+            direction[1] = 1
+        
+        elif direction == [0,-1]:
+            direction[0] = 1
+            direction[1] = 0
+        
+        elif direction == [-1,0]:
+            direction[0] = 0
+            direction[1] = -1
+
+matrix = int(input())
+
+apple = [[False] * matrix for _ in range(matrix)] 
+
+appleNum = int(input())
+
+for i in range(appleNum):
+    row,column = map(int,input().split())
+    apple[row-1][column-1] = True
+
+dirChangeCount = int(input())
+dirInfo = []
+
+for i in range(dirChangeCount):
+    time,direction = input().split()
+    time = int(time)
+    dirInfo.append([time,direction])
+
+snake = []
+direction = [0,1]
+x = 0
+y = 0
+time = 0
+
+snake.append([x,y])
+
+while True:
+    time += 1
+
+    x = x + direction[0]
+    y = y + direction[1]
+    
+    if gameOver(x,y,snake):
+        print(time)
+        break
+
+    snake.append([x,y])
+    
+
+    if not catchApple(x,y,apple):
+        snake.pop(0)
+
+
+    for i in dirInfo:
+
+        if time == i[0]:
+            changeDir(direction,i)
